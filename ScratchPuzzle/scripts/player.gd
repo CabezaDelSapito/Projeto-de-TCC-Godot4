7f7124b3fa92f355607ffa7be6d4dd0891c2c7e9
José Olímpio <olimpiokbz@gmail.com>
2024-12-12 14:39:49 -0300

    player animation

--- ScratchPuzzle/scripts/player.gd
+++ ScratchPuzzle/scripts/player.gd
@@ -1,12 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 350.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_jumping := false
 
+@onready var texture := $AnimatedSprite2D as AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,13 +18,22 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		is_jumping = true
+	elif is_on_floor():
+		is_jumping = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		texture.scale.x = -direction
+		if !is_jumping:
+			texture.play('run')
+	elif is_jumping:
+		texture.play('jump')
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		texture.play('idle')
 
 	move_and_slide()
