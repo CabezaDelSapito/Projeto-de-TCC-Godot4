a0e7a5e05c1bea90a4e85ce198d3702d1ddf0527
José Olímpio <olimpiokbz@gmail.com>
2024-12-12 11:26:40 -0300

    Botao ( Limpar + Executar )

--- ScratchPuzzle/scripts/draggable.gd
+++ ScratchPuzzle/scripts/draggable.gd
@@ -4,10 +4,15 @@ extends TextureRect
 
 func _get_drag_data(at_position: Vector2) -> Variant:
 	var data = [self, CommandType]
+	visible = false
 	var preview = TextureRect.new()
 	preview.texture = texture
 	set_drag_preview(preview)
 	
 	return data
-	
+
+func _notification(notification_type) -> void:
+		match notification_type:
+			NOTIFICATION_DRAG_END:
+				visible = true
 
