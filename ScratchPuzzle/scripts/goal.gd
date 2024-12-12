d1a28e107cb608f1746e53203b7e909fa0b8f608
José Olímpio <olimpiokbz@gmail.com>
2024-12-12 16:31:54 -0300

    transição de level

--- ScratchPuzzle/scripts/goal.gd
+++ ScratchPuzzle/scripts/goal.gd
@@ -4,9 +4,8 @@ extends Area2D
 @export var next_level : String = ""
 @onready var goal: Area2D = $"."
 
-func _unhandled_input(event):
-	if goal.get_overlapping_bodies().size() > 0 && event.is_action_pressed("interact") && !next_level == "":
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == 'player' && !next_level == "":
 		transition.change_scene(next_level)
 	else:
 		print("No Scene Loaded")
-
