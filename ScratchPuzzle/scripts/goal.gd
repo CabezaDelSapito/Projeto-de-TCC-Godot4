af4e90bdcbfe9c8cd6596f2c9b56185602e19453
José Olímpio <olimpiokbz@gmail.com>
2025-02-13 18:19:11 -0300

    Ajuste Goal + Transition

--- ScratchPuzzle/scripts/goal.gd
+++ ScratchPuzzle/scripts/goal.gd
@@ -4,8 +4,13 @@ extends Area2D
 @export var next_level : String = ""
 @onready var goal: Area2D = $"."
 
+@onready var player: CharacterBody2D = $"../player"
+
+
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == 'player' && !next_level == "":
+		player.parar()
 		transition.change_scene(next_level)
 	else:
 		print("No Scene Loaded")
--- ScratchPuzzle/scripts/transition.gd
+++ ScratchPuzzle/scripts/transition.gd
@@ -3,10 +3,13 @@ extends CanvasLayer
 @onready var color_rect: ColorRect = $ColorRect
 
 func change_scene(path, delay = 0.5):
+	visible = 1
 	var scene_transition = get_tree().create_tween()
 	scene_transition.tween_property(color_rect, 'threshold', 1.0, 0.5).set_delay(delay)
 	await scene_transition.finished
+	visible = 0
 	assert(get_tree().change_scene_to_file(path) == OK)
+
 
 func show_new_scene():
 	var show_transition = get_tree().create_tween()
