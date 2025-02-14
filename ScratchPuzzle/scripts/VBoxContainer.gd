67b872a66d92c050f1a6e0ff2de9e84bfc2ac8e3
José Olímpio <olimpiokbz@gmail.com>
2025-02-14 19:57:20 -0300

    correção de erro: duplicar ou apagar bloco de comando

--- ScratchPuzzle/scripts/VBoxContainer.gd
+++ ScratchPuzzle/scripts/VBoxContainer.gd
@@ -1,6 +1,8 @@
-extends VBoxContainer
+extends MarginContainer
 
 @export_enum("Andar", "Virar", "Pular") var CommandType = 0
+
+@onready var target_node = find_child("VBoxContainer")
 
 func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
 	#if data[1] == CommandType:
@@ -8,6 +10,8 @@ func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
 	#return false
 
 func _drop_data(_at_position: Vector2, data: Variant) -> void:
-	data[0].get_parent().remove_child(data[0])
+	if data[0].get_parent() != target_node:
+		#target_node.add_child(data[0].duplicate())
+		data[0].get_parent().remove_child(data[0])
 	#add_child(data[0])
 	#data[0].global_position = global_position
--- ScratchPuzzle/scripts/dropArea.gd
+++ ScratchPuzzle/scripts/dropArea.gd
@@ -1,8 +1,6 @@
 extends PanelContainer
 
 @onready var target_node = find_child("GridContainer")
-#@onready var map_o1 = get_tree().current_scene.get_node_or_null("MarginContainer/HBoxContainer/LevelMenu/MarginContainer/LevelArea/map_01")
-#@onready var player = map_o1.get_node_or_null("player") if map_o1 else null
 
 @onready var level_area = get_tree().current_scene.get_node("MarginContainer/HBoxContainer/LevelMenu/MarginContainer/LevelArea")
 @onready var map = level_area.get_child(0) if level_area.get_child_count() > 0 else null
@@ -13,17 +11,11 @@ extends PanelContainer
 func _can_drop_data(_at_position: Vector2, _data: Variant) -> bool:
 	return true  # Pode modificar conforme necessário
 
-func _drop_data(_at_position: Vector2, data: Variant) -> void:
-	# Acha o nó desejado onde o filho deve ser adicionado
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+	# Verifica se a posição mudou antes de adicionar o filho ao nó
+	if data[0].get_parent() != target_node:
+		target_node.add_child(data[0].duplicate())
 	
-	# Remove o filho atual do nó original
-	#data[0].get_parent().remove_child(data[0])
-	
-	# Adiciona o filho no nó desejado
-	target_node.add_child(data[0].duplicate())
-	
-	# Ajusta a posição global do filho
-	#data[0].global_position = global_position
 
 func _on_execute_button_pressed() -> void:
 	var comandos = []
@@ -48,4 +40,4 @@ func executar_sequencial(comandos):
 				player.pular()
 				
 				
-		await get_tree().create_timer(0.5).timeout  # Espera entre comandos
+		await get_tree().create_timer(0.6).timeout  # Espera entre comandos
