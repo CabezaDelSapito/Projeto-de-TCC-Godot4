aed57fa10bbb308d637d6f17b7abf0712afb991a
José Olímpio <olimpiokbz@gmail.com>
2025-03-25 22:08:05 -0300

    inicio level 6

--- ScratchPuzzle/scripts/baseLevel.gd
+++ ScratchPuzzle/scripts/baseLevel.gd
@@ -10,7 +10,8 @@ extends PanelContainer
 	"level_2": preload("res://levels/level_2.tscn"),
 	"level_3": preload("res://levels/level_3.tscn"),
 	"level_4": preload("res://levels/level_4.tscn"),
-	"level_5": preload("res://levels/level_5.tscn")
+	"level_5": preload("res://levels/level_5.tscn"),
+	"level_6": preload("res://levels/level_6.tscn")
 }
 @export var commands: Dictionary = {
 	"level_1": [preload("res://commands/ComandoAndar.tscn"), preload("res://commands/ComandoVirar.tscn"), preload("res://commands/ComandoPular.tscn"),preload("res://commands/ComandoParar.tscn"), preload("res://commands/ComandoEsperar.tscn"),preload("res://commands/ComandoRepetir.tscn"),preload("res://commands/ComandoSe.tscn")],
@@ -21,7 +22,7 @@ extends PanelContainer
 }
 
 var current_map = null
-var current_level = "level_5"
+var current_level = "level_1"
 var player = null
 
 func _ready():
@@ -46,7 +47,7 @@ func load_map(level_name: String):
 	
 	if level_name in maps:
 		current_map = maps[level_name].instantiate()
-		current_map.scale = Vector2(1, 1)  # Ajusta a escala do mapa
+		current_map.scale = Vector2(1.5, 1.5)  # Ajusta a escala do mapa
 		map_container.add_child(current_map)
 		
 		player = current_map.get_node("player")
