eee04755a600f1f7e0200710098414f3457ebf77
José Olímpio <olimpiokbz@gmail.com>
2025-04-01 09:20:04 -0300

    Level 6
    definindo level 6 para apresentar o comando Repetir

--- ScratchPuzzle/scripts/baseLevel.gd
+++ ScratchPuzzle/scripts/baseLevel.gd
@@ -12,19 +12,21 @@ extends PanelContainer
 	"level_3": preload("res://levels/level_3.tscn"),
 	"level_4": preload("res://levels/level_4.tscn"),
 	"level_5": preload("res://levels/level_5.tscn"),
-	"level_6": preload("res://levels/level_6.tscn")
+	"level_6": preload("res://levels/level_6.tscn"),
+	"level_7": preload("res://levels/level_7.tscn")
 }
 @export var commands: Dictionary = {
 	"level_1": [preload("res://commands/ComandoAndar.tscn")],
 	"level_2": [preload("res://commands/ComandoAndar.tscn"), preload("res://commands/ComandoVirar.tscn") ],
 	"level_3": [preload("res://commands/ComandoAndar.tscn"), preload("res://commands/ComandoVirar.tscn"), preload("res://commands/ComandoEsperar.tscn")],
-	"level_4": [preload("res://commands/ComandoAndar.tscn"), preload("res://commands/ComandoVirar.tscn"), preload("res://commands/ComandoPular.tscn"), preload("res://commands/ComandoEsperar.tscn")],
-	"level_5": [preload("res://commands/ComandoAndar.tscn"), preload("res://commands/ComandoVirar.tscn"), preload("res://commands/ComandoPular.tscn"),preload("res://commands/ComandoParar.tscn"), preload("res://commands/ComandoEsperar.tscn")],
-	"level_6": [preload("res://commands/ComandoAndar.tscn"), preload("res://commands/ComandoVirar.tscn"), preload("res://commands/ComandoPular.tscn"),preload("res://commands/ComandoParar.tscn"), preload("res://commands/ComandoEsperar.tscn"),preload("res://commands/ComandoRepetir.tscn"),preload("res://commands/ComandoSe.tscn")],
+	"level_4": [preload("res://commands/ComandoAndar.tscn"), preload("res://commands/ComandoVirar.tscn"), preload("res://commands/ComandoEsperar.tscn"), preload("res://commands/ComandoPular.tscn")],
+	"level_5": [preload("res://commands/ComandoAndar.tscn"), preload("res://commands/ComandoVirar.tscn"), preload("res://commands/ComandoEsperar.tscn"), preload("res://commands/ComandoPular.tscn"),preload("res://commands/ComandoParar.tscn")],
+	"level_6": [preload("res://commands/ComandoAndar.tscn"), preload("res://commands/ComandoVirar.tscn"), preload("res://commands/ComandoEsperar.tscn"), preload("res://commands/ComandoPular.tscn"),preload("res://commands/ComandoParar.tscn"),preload("res://commands/ComandoRepetir.tscn")],
+	"level_7": [preload("res://commands/ComandoAndar.tscn"), preload("res://commands/ComandoVirar.tscn"), preload("res://commands/ComandoEsperar.tscn"), preload("res://commands/ComandoPular.tscn"),preload("res://commands/ComandoParar.tscn"),preload("res://commands/ComandoRepetir.tscn"),preload("res://commands/ComandoSe.tscn")],
 }
 
 var current_map = null
-var current_level = "level_4"
+var current_level = "level_1"
 var player = null
 
 func _ready():
