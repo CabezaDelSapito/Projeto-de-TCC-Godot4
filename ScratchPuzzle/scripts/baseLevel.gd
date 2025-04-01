3b9ee70e1b8646412f3f81dcc01cdd514bf2e4b6
José Olímpio <olimpiokbz@gmail.com>
2025-04-01 08:45:46 -0300

    Novo level 4 + level 5
    Level 4 refeito para ficar mais bonito e parecido com level 5
    
    level 5 - apresenta o comando "parar" e a mecanica de "espinhos", onde quando o player colidir com os espinhos ele morre e reinicia o mapa

--- ScratchPuzzle/scripts/baseLevel.gd
+++ ScratchPuzzle/scripts/baseLevel.gd
@@ -19,12 +19,12 @@ extends PanelContainer
 	"level_2": [preload("res://commands/ComandoAndar.tscn"), preload("res://commands/ComandoVirar.tscn") ],
 	"level_3": [preload("res://commands/ComandoAndar.tscn"), preload("res://commands/ComandoVirar.tscn"), preload("res://commands/ComandoEsperar.tscn")],
 	"level_4": [preload("res://commands/ComandoAndar.tscn"), preload("res://commands/ComandoVirar.tscn"), preload("res://commands/ComandoPular.tscn"), preload("res://commands/ComandoEsperar.tscn")],
-	"level_5": [preload("res://commands/ComandoAndar.tscn"), preload("res://commands/ComandoVirar.tscn"), preload("res://commands/ComandoPular.tscn"),preload("res://commands/ComandoParar.tscn"), preload("res://commands/ComandoEsperar.tscn"),preload("res://commands/ComandoRepetir.tscn"),preload("res://commands/ComandoSe.tscn")],
+	"level_5": [preload("res://commands/ComandoAndar.tscn"), preload("res://commands/ComandoVirar.tscn"), preload("res://commands/ComandoPular.tscn"),preload("res://commands/ComandoParar.tscn"), preload("res://commands/ComandoEsperar.tscn")],
 	"level_6": [preload("res://commands/ComandoAndar.tscn"), preload("res://commands/ComandoVirar.tscn"), preload("res://commands/ComandoPular.tscn"),preload("res://commands/ComandoParar.tscn"), preload("res://commands/ComandoEsperar.tscn"),preload("res://commands/ComandoRepetir.tscn"),preload("res://commands/ComandoSe.tscn")],
 }
 
 var current_map = null
-var current_level = "level_1"
+var current_level = "level_4"
 var player = null
 
 func _ready():
